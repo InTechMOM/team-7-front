ec5b97baaf686e1686c80f27e0799c30e82a6dce
Ana Maria Vergara <anavergara05@gmail.com>
2023-04-27 20:13:43 -0500

    Se agrega alerta @ y # caracteres-login

--- script.js
+++ script.js
@@ -10,3 +10,15 @@ circles.forEach(circle => {
     circle.classList.add('active');
   });
 });
+
+
+
+/*CUADRO DE CORREO: Límitar extensión de correo y caracteres del correo*/
+
+function validarEmail() {
+  var email = document.getElementById("email").value;
+  if (email.indexOf("@") == -1) {
+    alert("Por favor, ingrese una dirección de correo electrónico válida que contenga un signo @.");
+    return false;
+  }
+}
